fbdff67aab1b06dc74553cf1a8f6603c27b4f9b2
Abramov21 <104825295+Abramov21@users.noreply.github.com>
2022-09-22 15:56:32 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,61 @@
-import Profile from './profiles';
-import Statistics from './statistics';
-import Friend from './friends';
-import Transactions from './transactions';
+import { React, Component } from 'react';
+import Section from './Section';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification';
+import FeedbackOptions from './FeedbackOptions';
 
-import user from './data/user.json';
-import data from './data/data.json';
-import friends from './data/friends.json';
-import transactions from './data/transactions.json';
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-export const App = () => {
-  return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <Friend friends={friends} />
-      <Transactions items={transactions} />
-    </div>
-  );
-};
+
+  countTotalFeedback(){
+    const { good, bad, neutral} = this.state;
+    return good+bad+neutral
+  }
+
+  countPositiveFeedbackPercentage(){
+    const { good, bad, neutral} = this.state;
+    const PositivePercentage = Math.round(
+      (good / (good + bad + neutral)) * 100
+    );
+    return PositivePercentage;
+  }
+
+  handleIncrement = (option) => {
+    this.setState((prevState) => {
+      return {
+        [option]: prevState[option] +1,
+      }
+     })
+  }
+
+
+  render() {
+    const { good,bad,neutral} =this.state;
+    const options = Object.keys(this.state)
+    const total = this.countTotalFeedback()
+    const positivePercentage = this.countPositiveFeedbackPercentage()
+
+    return (
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} setFeedback={this.handleIncrement} />
+
+        {total > 0 ? (
+            <Statistics
+            good={good}
+            bad={bad}
+            neutral={neutral}
+            total ={total}
+            positivePercentage ={positivePercentage}
+            />
+          ) : (
+            <Notification massege="There is no feedback" />
+          )}
+      </Section>
+      )
+  }
+}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,34 +1,24 @@
 import PropTypes from 'prop-types';
-import css from './statistics.module.css';
+// import css from 'react';
 
-const Statistics = ({title,stats}) =>{
+const Statistics = ({ good, bad, neutral, total, positivePercentage }) => {
   return (
-    <section className={css.statistics}>
-    {title && <h2 className={css.title}>{title}</h2>}
-
-    <ul className={css.stat__list}>
-      {stats.map(({ id, label, percentage }) => (
-        <li
-          key={id}
-          className={css.item}
-        >
-          <span className={css.label}>{label}</span>
-          <span className={css.percentage}>{percentage} %</span>
-        </li>
-      ))}
-    </ul>
-  </section>
-);
-}
+    <>
+      <p>Good: {good}</p>
+      <p>Bad: {bad}</p>
+      <p>Neutral {neutral}</p>
+      <p>Total: {total}</p>
+      <p>Positiv: {positivePercentage} %</p>
+    </>
+  );
+};
 
 export default Statistics;
 
-
 Statistics.propTypes = {
-  title: PropTypes.string,
-  stats: PropTypes.arrayOf( PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    percentage: PropTypes.number.isRequired,
-  })).isRequired,
+  positivePercentage: PropTypes.number,
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
 };
